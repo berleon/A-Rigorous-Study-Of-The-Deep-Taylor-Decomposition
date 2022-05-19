60539db5a77c6da7b751310f35b7dec6a9536ba7
berleon <github@leon-sixt.de>
2022-05-19 09:03:01 +0200

    Ensure NLTK_DATA ist set

--- lrp_relations/preprocess_clevr.py
+++ lrp_relations/preprocess_clevr.py
@@ -1,4 +1,5 @@
 import dataclasses
+import os
 
 import savethat
 
@@ -20,5 +21,11 @@ class PreprocessCLEVR(
     savethat.Node[PreprocessCLEVRArgs, PreprocessCLEVRResult]
 ):
     def _run(self):
+
+        if "NLTK_DATA" not in os.environ:
+            raise RuntimeError(
+                "Please set the environment variable NLTK_DATA "
+                "to the location of the NLTK data."
+            )
         preprocess.main(self.args.dataset, self.args.only_questions)
         return PreprocessCLEVRResult()
