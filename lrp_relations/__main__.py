da3475d77ae81ef32dabd92f844aa29071b9fcfc
berleon <github@leon-sixt.de>
2022-05-19 09:00:28 +0200

    Use locally saved credentials

--- lrp_relations/__main__.py
+++ lrp_relations/__main__.py
@@ -1,7 +1,10 @@
-import coverage
+from pathlib import Path
+
 import savethat
 
-# ensures coverage is also collected for subprocesses
-coverage.process_startup()
-
-savethat.run_main("lrp_relations")
+if __name__ == "__main__":
+    pkg_dir = Path(__file__).parent
+    creds = pkg_dir.parent.parent / "savethat_credentials.toml"
+    savethat.run_main(
+        "lrp_relations", credential_file=creds if creds.exists() else None
+    )
