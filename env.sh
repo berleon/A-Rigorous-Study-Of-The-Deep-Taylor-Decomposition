79f82247659dec58c2823cae10884e0ae7c5f929
berleon <github@leon-sixt.de>
2022-05-19 08:59:20 +0200

    Export dataset paths and buckets

---
+++ env.sh
@@ -0,0 +1,7 @@
+
+export DATA_DIR=`realpath ../data`
+export NLTK_DATA=`realpath ../data/nltk_data`
+export CLEVR=`realpath ../data/CLEVR_v1.0/`
+export CLEVR_XAI=`realpath ../data/CLEVR-XAI_v1.0/`
+export BUCKET="models-7njma-7rb8w-tpr43f"
+export B2_PREFIX="b2://$BUCKET/lrp_relations"
