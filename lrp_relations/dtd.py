ffa13dc1217110dd995d12f15bc551fa51ec7758
berleon <github@leon-sixt.de>
2022-05-24 14:18:32 +0200

    Update DTD

--- lrp_relations/dtd.py
+++ lrp_relations/dtd.py
@@ -1,5 +1,39 @@
+from typing import Callable, Union
+
 import torch
 from torch import nn
+from torch.utils import hooks
+
+
+class record_all_outputs:
+    """A context manager that stores all outputs of all layers."""
+
+    def __init__(self, module: nn.Module):
+        self.module = module
+        self.outputs: dict[nn.Module, list[torch.Tensor]] = {}
+        self.handles: list[hooks.RemovableHandle] = []
+
+    def __enter__(
+        self,
+    ) -> dict[nn.Module, list[torch.Tensor]]:
+        def hook(
+            module: nn.Module, inputs: tuple[torch.Tensor], output: torch.Tensor
+        ) -> None:
+            if module not in self.outputs:
+                self.outputs[module] = []
+
+            self.outputs[module].append(output)
+
+        self.module.apply(
+            lambda module: self.handles.append(
+                module.register_forward_hook(hook)
+            )
+        )
+        return self.outputs
+
+    def __exit__(self, *args):
+        for handle in self.handles:
+            handle.remove()
 
 
 class LinearReLU(nn.Module):
@@ -21,6 +55,12 @@ class LinearReLU(nn.Module):
 class TwoLayerMLP(nn.Module):
     def __init__(self, input_size: int, hidden_size: int, output_size: int):
         super().__init__()
+
+        # store parameters
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+
         self.layer1 = LinearReLU(input_size, hidden_size)
         self.layer2 = LinearReLU(hidden_size, output_size)
 
@@ -28,8 +68,40 @@ class TwoLayerMLP(nn.Module):
         return self.layer2(self.layer1(x))
 
 
+class NLayerMLP(nn.Module):
+    def __init__(
+        self, n_layers: int, input_size: int, hidden_size: int, output_size: int
+    ):
+        super().__init__()
+
+        # store parameters
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.layers = []
+        for i in range(n_layers):
+            if i == 0:
+                layer = LinearReLU(input_size, hidden_size)
+            elif i == n_layers - 1:
+                layer = LinearReLU(hidden_size, output_size)
+            else:
+                layer = LinearReLU(hidden_size, hidden_size)
+            setattr(self, f"layer{i + 1}", layer)
+            self.layers.append(layer)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
 def root_point(
-    x: torch.Tensor, layer: LinearReLU, j: int, rule: str = "z+"
+    x: torch.Tensor,
+    layer: LinearReLU,
+    j: int,
+    rule: str = "z+",
+    gamma: Union[float, torch.Tensor] = 1.0,
 ) -> torch.Tensor:
     """Return the DTD root point.
 
@@ -37,8 +109,9 @@ def root_point(
         x: Input tensor.
         layer: Layer to compute the root point.
         j: Index of which neuron to compute the root point.
-        rule: Rule to compute the root point (supported: `z+` and `w2`).
-
+        rule: Rule to compute the root point (supported: `z+`, `w2`, and
+            `gamma`).
+        gamma: Scaling factor for DTD gamma rule
     Returns:
         A root point `r` with the property `layer(r)[j] == 0`.
     """
@@ -46,20 +119,157 @@ def root_point(
     b = layer.linear.bias  # [out]
     x  # [b, in]
 
-    #  See 1.3 and 1.4 in DTD Appendix
-    if rule == "z+":
-        v = x * (w[j, :] >= 0).unsqueeze(0)
-    elif rule == "w2":
-        v = w[j, :].unsqueeze(0)
-    elif rule == "zB":
-        raise NotImplementedError()
-    else:
-        raise ValueError()
-
     w_j = w[j, :].unsqueeze(0)
     b_j = b[j]
+    indicator_w_j_pos = (w_j >= 0).float()  # [1, in]
+    #  See 1.3 and 1.4 in DTD Appendix
+    if rule == "z+":
+        v = x * indicator_w_j_pos
+    elif rule == "w2":
+        v = w_j
+    elif rule == "x":
+        v = x
+    elif rule == "zB":
+        raise NotImplementedError()
+    elif rule == "0":
+        return 0 * x
+    elif rule in ["gamma", "γ"]:
+        # From: Layer-Wise Relevance Propagation: An Overview
+        # https://www.doi.org/10.1007/978-3-030-28954-6_10
+        # In section: 10.2.3 under Relation to LRP-0//γ
+
+        # line eq: x - t * x * (1 + gamma * indicator_w_j_pos)
+        # the intersection with the ReLU hinge is done below
+        v = x * (1 + gamma * indicator_w_j_pos)
+    else:
+        raise ValueError()
     assert torch.allclose(layer.linear(x)[:, j].unsqueeze(1), x @ w_j.t() + b_j)
 
     #  This is equation (4) in DTD Appendix
     t = (x @ w_j.t() + b_j).sum(1) / (v @ w_j.t()).sum(1)
-    return x - t.unsqueeze(1) * v
+    # print("xw", (x @ w_j.t() + b_j).sum(1))
+    # print("vw", (v @ w_j.t()).sum(1))
+    # assert t.isnan().sum() == 0
+
+    t[~t.isfinite()] = 0.0
+
+    root_point = x - t.unsqueeze(1) * v
+    return root_point
+
+
+def get_relevance_hidden(
+    net: TwoLayerMLP,
+    x: torch.Tensor,
+    j: int = 0,
+    rule: str = "z+",
+    gamma: Union[float, torch.Tensor] = 1.0,
+) -> torch.Tensor:
+    return get_relevance_hidden_and_root(net, x, j, rule, gamma)[0]
+
+
+def get_relevance_hidden_and_root(
+    net: TwoLayerMLP,
+    x: torch.Tensor,
+    j: int = 0,
+    rule: str = "z+",
+    gamma: Union[float, torch.Tensor] = 1.0,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """Return the relevance of each neuron in the hidden layer and the
+    corresponding root point.
+
+    This applies the given DTD rule to the final layer.
+
+    Args:
+        net: TwoLayerMLP network.
+        x: Input tensor.
+        j: Index of which neuron in the final layer to compute the relevance.
+        rule: Rule to compute the root point
+        gamma: Scaling factor for DTD gamma rule
+
+    Returns:
+        A tuple of two tensors: (the relevance of each neuron in the hidden
+            layer, the corresponding root point).
+    """
+    with record_all_outputs(net) as outputs:
+        net(x)
+
+    # outputs
+    hidden = outputs[net.layer1][0]
+    hidden_root = root_point(hidden, net.layer2, j, rule=rule, gamma=gamma)
+
+    assert torch.isnan(hidden_root).sum() == 0
+
+    output_root = net.layer2.linear(hidden_root)
+
+    assert output_root.isnan().sum() == 0
+    (rel_grad,) = torch.autograd.grad(
+        output_root,
+        hidden_root,
+        grad_outputs=torch.ones_like(output_root),
+        retain_graph=True,
+    )
+    assert rel_grad.isnan().sum() == 0
+    rel_hidden = rel_grad * (hidden - hidden_root)
+    return rel_hidden, hidden_root
+
+
+def find_input_root_point(
+    net: TwoLayerMLP,
+    x: torch.Tensor,
+    j: int,
+    relevance_fn: Callable[[TwoLayerMLP, torch.Tensor], torch.Tensor],
+    n_samples: int = 1_000,
+    plot: bool = False,
+) -> torch.Tensor:
+    """Return the root point of the input layer.
+
+    Args:
+        net: TwoLayerMLP network.
+        x: Input tensor.
+        j: Index of which neuron in the hidden layer to compute the relevance.
+        relevance_fn: Function to compute the relevance of each neuron in the
+            hidden layer.
+        n_samples: Number of samples to find the root point.
+
+    Returns:
+        The root point of the input layer.
+    """
+    assert x.size(0) == 1
+    length = 3 * torch.randn(n_samples, 1) + 5
+    x_search = x + torch.randn(n_samples, x.size(1)) * length
+
+    rel_hidden_search = relevance_fn(net, x_search)
+
+    idx = rel_hidden_search[:, j].abs().argmin()
+
+    steps = torch.linspace(-0.3, 1.05, 100).view(-1, 1)
+
+    smallest_x = x_search[idx].unsqueeze(0)
+    x_line_search = smallest_x + steps * (x - smallest_x)
+
+    rel_hidden_line_search = relevance_fn(net, x_line_search)
+
+    if plot:
+        import matplotlib.pyplot as plt
+
+        plt.plot(rel_hidden_line_search.detach().cpu().numpy()[:, j])
+        plt.show()
+    root_idx = (rel_hidden_line_search[:, j] <= 1e-2).nonzero().max()
+    return x_line_search[root_idx]
+
+
+def almost_unique(x: torch.Tensor, atol: float = 1e-5) -> torch.Tensor:
+    """Returns indices of unique elements in x, with tolerance atol."""
+    sort_idx = torch.argsort(torch.norm(x, dim=1))
+    x_sorted = x[sort_idx]
+
+    dist = torch.pairwise_distance(x_sorted[:-1], x_sorted[1:])
+
+    below_atol = (dist > atol).long()
+
+    unique = dist.new_zeros(dist.shape[0] + 1, dtype=torch.long)
+    unique[1:] = below_atol.cumsum(dim=0)
+
+    unique_mask = x.new_zeros(size=(len(x),), dtype=torch.long)
+    unique_mask[sort_idx] = unique
+    return unique_mask
--- tests/test_dtd.py
+++ tests/test_dtd.py
@@ -9,13 +9,73 @@ def test_dtd_root():
 
     idx = 0
     x = (0.25 * torch.randn(2, 3, requires_grad=True) + 1).clamp(min=0)
-    x_root = dtd.root_point(x, net.layer1, idx)
 
-    x_root.shape, x.shape
-    print("x", x)
-    print("x_root", x_root)
-    print("out", net.layer1.linear(x)[:, idx].tolist())
-    print("out root", net.layer1.linear(x_root)[:, idx].tolist())
+    for rule in ["0", "z+", "w2", "gamma"]:
+        x_root = dtd.root_point(x, net.layer1, idx, rule=rule)
 
-    root_output = net.layer1.linear(x_root)[:, idx]
-    assert torch.allclose(root_output, torch.zeros_like(root_output))
+        x_root.shape, x.shape
+        print("x", x)
+        print("x_root", x_root)
+        print("out", net.layer1.linear(x)[:, idx].tolist())
+        print("out root", net.layer1.linear(x_root)[:, idx].tolist())
+
+        root_output = net.layer1.linear(x_root)[:, idx]
+        assert torch.allclose(
+            root_output, torch.zeros_like(root_output), atol=1e-6
+        )
+
+
+def test_dtd_input_root():
+    torch.manual_seed(2)
+    net = dtd.TwoLayerMLP(input_size=3, hidden_size=5, output_size=1)
+
+    weight = net.layer2.linear.weight.data.clone()
+    weight[:, :2] = 5 * weight.abs()[:, :2]
+    net.layer2.linear.weight.data = weight
+    net.layer2.linear.weight
+    torch.manual_seed(3)
+    x = (0.25 * torch.randn(1, 3, requires_grad=True) + 3).clamp(min=0)
+
+    rules = ["0", "z+", "w2", "gamma"]
+
+    for rule in rules:
+        print("-" * 80)
+        print("Rule:", rule)
+
+        def relevance_fn(net: dtd.TwoLayerMLP, x: torch.Tensor) -> torch.Tensor:
+            return dtd.get_relevance_hidden(net, x, rule=rule, gamma=1000)
+
+        with dtd.record_all_outputs(net) as x_outs:
+            logit_x = net(x)
+
+        rel_hidden = relevance_fn(net, x)
+        hidden_root = dtd.root_point(
+            x_outs[net.layer1][0], net.layer2, 0, rule=rule, gamma=1000
+        )
+        print("hidden_root", hidden_root)
+        print("x", x)
+        x_root = dtd.find_input_root_point(
+            net, x, 0, relevance_fn, n_samples=10_000
+        )
+        print("x_root", x_root)
+
+        print("relevance hidden", rel_hidden)
+
+        with dtd.record_all_outputs(net) as x_root_outs:
+            logit_x_root = net(x_root)
+
+        print("Logit x", logit_x.tolist())
+        print("Logit x_root", logit_x_root.tolist())
+
+        print("hidden with x", x_outs[net.layer1][0])
+        print("hidden with x root", x_root_outs[net.layer1][0])
+
+
+def test_almost_unique():
+    atol = 1e-5
+    x = torch.tensor(
+        [[0, 0.0], [0, 2 * atol], [0, 2.2 * atol], [0, 0.1 * atol], [1, 1.0]]
+    )
+
+    unique_idx = dtd.almost_unique(x, atol=atol).tolist()
+    assert unique_idx == [0, 1, 1, 0, 2]
